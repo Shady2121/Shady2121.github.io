8be959a77a5ba42174f51470616effa5b359e9cd
Shady <roobengg@gmail.com>
2020-01-16 21:46:21 +0300

    commit

---
+++ PremiumEcommerceebsiteTemplatePSD/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('.main-slider').slick({
+    //   setting-name: setting-value
+    dots: true
+    });
+  });
+  $(document).ready(function(){
+    $('.categories-slider').slick({
+    //   setting-name: setting-value
+    dots: true,
+    slidesToShow: 3,
+    arrows: true,
+    speed: 300
+    });
+  });
